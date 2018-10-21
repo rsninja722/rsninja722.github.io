4931efcad2ca760df96e9c719d7890c456098f23
rsninja <jnick722547@gmail.com>
2018-10-20 20:26:58 -0400

    i

--- nav.js
+++ nav.js
@@ -3,10 +3,11 @@ var navlist = [
     {name:"Ouro-fill-us",link:"https://rsninja722.github.io/ouro/indexo.html"},
     {name:"Circle TD",link:"https://rsninja722.github.io/circletd/indexr.html"},
     {name:"Space Shooter",link:"https://rsninja722.github.io/seperategames/indexspace.html"},
-    {name:"Strategic Digger",link:"https://rsninja722.github.io/digger.html"},
+    {name:"Strategic Digger",link:"https://rsninja722.github.io/seperategames/digger.html"},
     {name:"Seige Idle",link:"https://rsninja722.github.io/seperategames/sidgeidle.html"},
     {name:"Snake Game",link:"https://rsninja722.github.io/seperategames/snakegame.html"},
-    {name:"Crappy Clicker Game",link:"https://rsninja722.github.io/seperategames/5number idle.html"}
+    {name:"Crappy Clicker Game",link:"https://rsninja722.github.io/seperategames/5number idle.html"},
+    {name:"Home",link:"https://rsninja722.github.io/index.html"}
 ]
 //style
 var sty = document.createElement("style");
