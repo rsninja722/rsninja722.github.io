3b74d66215b494576ac4dd4e081de013fd479154
rsninja <jnick722547@gmail.com>
2018-10-20 20:15:07 -0400

    there we go

--- nav.js
+++ nav.js
@@ -1,6 +1,7 @@
 
 var navlist = [
-    {name:"Circle TD",link:"https://rsninja722.github.io/index.html"},
+    {name:"Ouro-fill-us",link:"https://rsninja722.github.io/ouro/index.html"},
+    {name:"Circle TD",link:"https://rsninja722.github.io/circletd/index.html"},
     {name:"Space Shooter",link:"https://rsninja722.github.io/seperategames/indexspace.html"},
     {name:"Strategic Digger",link:"https://rsninja722.github.io/digger.html"},
     {name:"Seige Idle",link:"https://rsninja722.github.io/seperategames/sidgeidle.html"},
@@ -12,7 +13,7 @@ var sty = document.createElement("style");
 sty.innerHTML = `
 ul{list-style-type: none;margin: 0;padding: 0;width: 150px;}
 li a {display: block;color: #000;padding: 8px 8px;text-decoration: none;}
-li a:hover {background-color: rgba(160, 160, 160, 0.356);color: rgb(84, 74, 177);}
+li a:hover {background-color: rgba(160, 160, 160, 1);color: rgb(84, 74, 177);}
 `
 document.getElementById("a83n7").appendChild(sty);
 //slightly broken image generator
