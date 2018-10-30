8dbdc9fd008df2be0afecc7fc54035027e624798
rsninja <jnick722547@gmail.com>
2018-10-29 20:00:13 -0400

    good4

--- nav.js
+++ nav.js
@@ -3,7 +3,7 @@ var navlist = [
     {name:"Ouro-fill-us",link:"https://rsninja722.github.io/seperategames/ouro/indexo.html"},
     {name:"Circle TD",link:"https://rsninja722.github.io/seperategames/circletd/indexr.html"},
     {name:"Space Shooter",link:"https://rsninja722.github.io/seperategames/indexspace.html"},
-    {name:"Space Shooter V2 (ALPHA)",link:"https://rsninja722.github.io/seperategames/indexspace.html"},
+    {name:"Space Shooter V2 (ALPHA)",link:"https://rsninja722.github.io/seperategames/spaceShooterV2/index.html"},
     {name:"Strategic Digger",link:"https://rsninja722.github.io/seperategames/digger.html"},
     {name:"Seige Idle",link:"https://rsninja722.github.io/seperategames/sidgeidle.html"},
     {name:"Snake Game",link:"https://rsninja722.github.io/seperategames/snakegame.html"},
