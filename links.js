02ea99123b9442f11d21c8e91543667f5d0bbc67
rsninja722 <jnick722547@gmail.com>
2019-09-01 22:45:53 -0400

    racing

--- links.js
+++ links.js
@@ -4,3 +4,14 @@ var links = [
     {title:"Slime Defender Clicker",desc:"Clicker/idle game where you allot currency to upgrade your slime shootin' turret.",thumb:"thumbnails/slime.PNG",link:"seperategames/slimeDefenderClicker/index.html"},
     {title:"Car Demo",desc:"really crappy car demo. WASD to move",thumb:"thumbnails/car.PNG",link:"games/racing/index.html"}
 ]
+/*
+don't forget to add to games
+
+<body style="margin: 0 0 0 0;">
+    <div style="width: 100%; height: 75px; background-color: #313131;">
+        <a href="/index.html" style="position:absolute;left:0;height: 75px;">
+            <img src="/home.png" style="height: 75px;image-rendering: pixelated;">
+        </a>
+        <img src="/logo.png" style="position:absolute;right:0;left:0;margin:auto;height: 75px;image-rendering: pixelated;">
+    </div>
+*/
