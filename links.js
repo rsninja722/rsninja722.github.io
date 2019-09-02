20dd4d7c5d38273b2c6c47242a26540caa516917
rsninja722 <jnick722547@gmail.com>
2019-09-01 22:44:36 -0400

    oops bugs

--- links.js
+++ links.js
@@ -1,5 +1,6 @@
 var links = [
-    {title:"Junk Escape",desc:`Play as a modular robot in a top down shooter, and escape the junk yard you crash landed in. Made in 1 week with NDS for Community Game Jam.`,thumb:"thumbnails/junk.PNG",link:"games/junkEscape/index.html"},
+    {title:"Junk Escape",desc:`Play as a modular robot in a top down shooter, and escape the junk yard you crash landed in. Made in 1 week with NDS for Community Game Jam.`,thumb:"thumbnails/junk.png",link:"games/junkEscape/index.html"},
     {title:"Ouro-fill-us",desc:"Puzzle game with the objective of eating your own snake tail. Made in 50hrs for Jim Jam #2.",thumb:"thumbnails/ouro.PNG",link:"seperategames/ouro/index.html"},
-    {title:"Slime Defender Clicker",desc:"Clicker/idle game where you allot currency to upgrade your slime shootin' turret.",thumb:"thumbnails/slime.PNG",link:"seperategames/slimeDefenderClicker/index.html"}
+    {title:"Slime Defender Clicker",desc:"Clicker/idle game where you allot currency to upgrade your slime shootin' turret.",thumb:"thumbnails/slime.PNG",link:"seperategames/slimeDefenderClicker/index.html"},
+    {title:"Car Demo",desc:"really crappy car demo. WASD to move",thumb:"thumbnails/car.PNG",link:"games/racing/index.html"}
 ]
