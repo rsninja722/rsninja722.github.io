42802f7215cc6c5285784e4756bd214755b7eb91
rsninja722 <jnick722547@gmail.com>
2020-08-04 22:51:59 -0400

    photos 2020.08.04

--- photos/files.js
+++ photos/files.js
@@ -1,13 +1,17 @@
 var files = [
-    "0.JPG", 
-    "1.JPG", 
-    "2.JPG", 
-    "3.JPG", 
-    "4.JPG", 
-    "5.JPG", 
-    "6.JPG", 
+    "0.JPG",
+    "1.JPG",
+    "2.JPG",
+    "3.JPG",
+    "4.JPG",
+    "5.JPG",
+    "6.JPG",
     "7.JPG",
     "8.JPG",
     "9.JPG",
-    "10.JPG"
+    "10.JPG",
+    "11.JPG",
+    "12.JPG",
+    "13.JPG",
+    "14.JPG"
 ];
