020ed8d208dc5ae6ea8b0adb77e2cd630b252ce8
rsninja722 <jnick722547@gmail.com>
2020-08-10 15:43:28 -0400

    moved photos

--- photos/thumb.bat
+++
@@ -1,5 +0,0 @@
-@set ASEPRITE="C:\Program Files (x86)\Steam\steamapps\common\Aseprite\Aseprite.exe"
-for %%f in (photosLarge\*) do (
-    %ASEPRITE% -b --oneframe %%f --scale 0.1 --save-as photosSmall\%%~nf.JPG
-    echo "%%~nf.JPG", >> fileList.txt
-)
