1459b3af8025695dd62c310db60219db90a0a3ac
rsninja722 <jnick722547@gmail.com>
2023-09-19 17:24:47 -0400

    visual touch ups, sections, added link

--- links.js
+++ links.js
@@ -1,30 +1,33 @@
-var links = [
+var projectLinks = [
+    {title:"Cellular Automata",desc:"Collection of cellular automata programs.",thumb:"thumbnails/automata.png",link:"notGames/cellularAutomata/index.html"},
+    {title:"JSFrick",desc:`Converter for my esoteric style of JavaScript programming which doesn't use any numbers, letters, or string literals`,thumb:"./thumbnails/jsfrick.jpg",link:"https://rsninja.dev/JSFrick/"},
+    {title:"Ninja Edit",desc:`Image editor geared towards pixel art. To start press "file > new".`,thumb:"thumbnails/pixeljs.png",link:"notGames/pixelJS/index.html"},
+    {title:"Mandelbrot",desc:"Fractal zoom animation and renders.",thumb:"thumbnails/mandel.png",link:"notGames/mandelbrot/mandelbrot.html"},
+    {title:"Photo Gallery",desc:`Pictures I've taken throughout my time doing photography.`,thumb:"thumbnails/gallery.png",link:"photos/index.html"},
+];
+
+var gameLinks = [
     {title:"Cabush's Kooky Climb",desc:"Can you help Cabush get back to the top ​of the hill? Or will you give up in rage because his controls are changing every 10 seconds ever since he had a bit too much fun with his friends",thumb:"thumbnails/Cabush.png",link:"https://rsninja.dev/CabushsKookyClimb/"},
     {title:"Junk Escape",desc:`Play as a modular robot in a top down shooter, and escape the junk yard you crash landed in. Made in 1 week with NDS for Community Game Jam.`,thumb:"thumbnails/junk.png",link:"games/junkEscape/index.html"},
     {title:"Ouro-fill-us",desc:"Puzzle game with the objective of eating your own snake tail. Made in 50hrs for Jim Jam #2.",thumb:"thumbnails/ouro.PNG",link:"seperategames/ouro/index.html"},
-    {title:"Cellular Automata",desc:"Collection of quick and messy cellular automata programs.",thumb:"thumbnails/automata.png",link:"notGames/cellularAutomata/index.html"},
     {title:"Textris",desc:"Text based generic block puzzle game that looks old, but plays like new.",thumb:"thumbnails/textris.png",link:"https://rsninja.dev/Textris/index.html"},
     {title:"Minesweeper",desc:"Clean off those dusty mines in this desktop classic.",thumb:"thumbnails/minesweeperthumb.png",link:"https://rsninja.dev/minesweeper/index.html"},
-    {title:"Photo Gallery",desc:`Check out the pictures I've taken throughout my time doing photography.`,thumb:"thumbnails/gallery.png",link:"photos/index.html"},
     {title:"Downloadables",desc:`Projects that have to be downloaded to play.`,thumb:"thumbnails/download.png",link:"notGames/downloadable/index.html"},
     {title:"Space Invaders",desc:"Defend your personal space from the Space Invaders, whoever they are...",thumb:"thumbnails/space.png",link:"games/spaceInvaders/index.html"},
     {title:"Slime Defender Clicker",desc:"Clicker/idle game where you allot currency to upgrade your slime shootin' turret.",thumb:"thumbnails/slime.PNG",link:"seperategames/slimeDefenderClicker/index.html"},
     {title:"Micromanaged Mike",desc:"multitasking platform/rage game made for Ludum Dare 46.",thumb:"thumbnails/mike.png",link:"https://rsninja.dev/LudumDare46"},
-    {title:"Ninja Edit",desc:`Janky but usable pixel art editor.`,thumb:"thumbnails/pixeljs.png",link:"notGames/pixelJS/index.html"},
-    {title:"tinytris",desc:"Simple, fast generic block puzzle game, with scalable board grid and resolution. Desktop and mobile versions available.",thumb:"thumbnails/tetris.PNG",link:"games/tetris/selectPlatform.html"},
-    {title:"Mari Oh",desc:"Quick project for my grade 11 philosophy class.",thumb:"thumbnails/marioh.png",link:"../mari-oh/index.html"},
-    {title:"Car Demo",desc:"Really crappy car demo. WASD to move.",thumb:"thumbnails/car.PNG",link:"games/racing/index.html"},
-    {title:"Mandelbrot",desc:"Fractal zoomer and renders.",thumb:"thumbnails/mandel.png",link:"notGames/mandelbrot/mandelbrot.html"},
     {title:"Zombies",desc:"Simple top down survival shooter.",thumb:"thumbnails/zombie.PNG",link:"seperategames/zombies/index.html"},
     {title:"Smaller Projects",desc:"Random projects that aren't worthy of the main page.",thumb:"thumbnails/meta.png",link:"smallProjects.html"},
-    {title:"archive",desc:"Links to all my old stuff that sucks too much to be on this page.",thumb:"thumbnails/archive.PNG",link:"archive.html"}
+    {title:"Archive",desc:"Links to all my old stuff that sucks too much to be on this page.",thumb:"thumbnails/archive.PNG",link:"archive.html"},
 ];
 
 var smallProjectLinks = [
     {title:"evolution",desc:"Sorta working evolution simulator. Makes the squares go to the right",thumb:"thumbnails/evo.png",link:"notGames/pathEvolution/index.html"},
     {title:"PPS",desc:"The thing you see in the background. Primordial particle simulator",thumb:"thumbnails/pps.png",link:"notGames/pps.html"},
     {title:"pixel PPS",desc:"Primordial particle simulator, but more, and with a quadtree",thumb:"thumbnails/pixelpps.png",link:"https://rsninja.dev/pixelPPS/index.html"},
-    {title:"Art Gen",desc:"Simply tool to quickly generate pixel art tiles.",thumb:"thumbnails/art.PNG",link:"seperategames/artgen/index.html"}
+    {title:"Art Gen",desc:"Simply tool to quickly generate pixel art tiles.",thumb:"thumbnails/art.PNG",link:"seperategames/artgen/index.html"},
+    {title:"Car Demo",desc:"Really crappy car demo. WASD to move.",thumb:"thumbnails/car.PNG",link:"games/racing/index.html"},
+    {title:"Mari Oh",desc:"Quick project for my grade 11 philosophy class.",thumb:"thumbnails/marioh.png",link:"../mari-oh/index.html"},
 ];
 
 /*
